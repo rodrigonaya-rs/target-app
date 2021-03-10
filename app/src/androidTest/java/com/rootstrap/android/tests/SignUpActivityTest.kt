47e74685824634645e82aaf4922e1d34820e7c5a
rodrigonaya-rs <rodrigo.naya@rootstrap.com>
2021-03-10 15:39:14 -0300

    Update sign up error handling

--- app/src/androidTest/java/com/rootstrap/android/tests/SignUpActivityTest.kt
+++ app/src/androidTest/java/com/rootstrap/android/tests/SignUpActivityTest.kt
@@ -1,8 +1,15 @@
 package com.rootstrap.android.tests
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.google.gson.Gson
 import com.rootstrap.android.R
+import com.rootstrap.android.network.models.User
 import com.rootstrap.android.network.models.UserSerializer
 import com.rootstrap.android.ui.activity.main.ProfileActivity
 import com.rootstrap.android.ui.activity.main.SignUpActivity
@@ -34,12 +41,7 @@ class SignUpActivityTest : BaseTests() {
         scenario.recreate()
         setServerDispatch(signUpDispatcher())
         val testUser = testUser()
-        scrollAndTypeText(R.id.first_name_edit_text, testUser.firstName)
-        scrollAndTypeText(R.id.last_name_edit_text, testUser.lastName)
-        scrollAndTypeText(R.id.email_edit_text, testUser.email)
-        scrollAndSelectItem(R.id.gender_spinner, testUser.gender)
-        scrollAndTypeText(R.id.password_edit_text, testUser.password)
-        scrollAndTypeText(R.id.password_confirmation_edit_text, testUser.password)
+        populateUserData(testUser)
         scrollAndPerformClick(R.id.sign_up_button)
         val user = sessionManager.user
         assertEquals(user, testUser)
@@ -50,6 +52,32 @@ class SignUpActivityTest : BaseTests() {
         }
     }
 
+    @Test
+    fun signUpErrorTest() {
+        scenario.recreate()
+        setServerDispatch(unsuccessfulSignUpDispatcher())
+        val testUser = testUser()
+        populateUserData(testUser)
+        scrollAndPerformClick(R.id.sign_up_button)
+        onView(withText(R.string.error))
+            .inRoot(isDialog())
+            .check(matches(isDisplayed()))
+        onView(withText(R.string.ok)).perform(click())
+        activity.runOnUiThread {
+            val current = currentActivity()
+            assertEquals(SignUpActivity::class.java.name, current::class.java.name)
+        }
+    }
+
+    private fun populateUserData(user: User) {
+        scrollAndTypeText(R.id.first_name_edit_text, user.firstName)
+        scrollAndTypeText(R.id.last_name_edit_text, user.lastName)
+        scrollAndTypeText(R.id.email_edit_text, user.email)
+        scrollAndSelectItem(R.id.gender_spinner, user.gender)
+        scrollAndTypeText(R.id.password_edit_text, user.password)
+        scrollAndTypeText(R.id.password_confirmation_edit_text, user.password)
+    }
+
     private fun signUpDispatcher(): Dispatcher {
         return object : Dispatcher() {
             override fun dispatch(request: RecordedRequest): MockResponse {
@@ -58,6 +86,17 @@ class SignUpActivityTest : BaseTests() {
                     mockServer.successfulResponse().setBody(
                         Gson().toJson(userResponse)
                     )
+                } else
+                    mockServer.notFoundResponse()
+            }
+        }
+    }
+
+    private fun unsuccessfulSignUpDispatcher(): Dispatcher {
+        return object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return if (request.path!!.contains("users")) {
+                    mockServer.customResponse(500)
                 } else
                     mockServer.notFoundResponse()
             }
